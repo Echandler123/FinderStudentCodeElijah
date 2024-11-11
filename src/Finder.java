2f04f0a533c44a153a31b5d2176730cf6c489691
Echandler123 <elijah.chandler@menloschool.org>
2024-11-11 15:10:29 -0800

    update

--- src/Finder.java
+++ src/Finder.java
@@ -16,27 +16,28 @@ public class Finder {
 
 
     private HashMap map;
+    // Constructor
     public Finder() {
         this.map = new HashMap();
     }
-
+    // Creates hash map of the keys and values given
     public void buildTable(BufferedReader br, int keyCol, int valCol) throws IOException {
-        // TODO: Complete the buildTable() function!
         String line = br.readLine();
         String[] current;
+        // Loop through each line
         while(line != null){
+            // Each line separated by commas so split the strings when a comma if found
             current = line.split(",");
             String key = current[keyCol];
             String val = current[valCol];
+            // Add key and values to hash map
             map.add(key,val);
             line = br.readLine();
         }
         br.close();
     }
-
+    // Searches hashmap for the value of a key given
     public String query(String key){
-        // TODO: Complete the query() function!
         return map.get(key);
     }
-
 }
--- src/HashMap.java
+++ src/HashMap.java
@@ -1,80 +1,87 @@
 public class HashMap {
     private static final String INVALID = "INVALID KEY";
     private static final int DEFAULT_TABLE_SIZE = 100000;
-    private int tableSize = 0;
+    private int tableSize;
     private int keyNum = 0;
     private String[] keys;
     private String[] values;
-    private String[] oldKeys;
-    private String[] oldVals;
-    private int hash = 0;
     private static final double maxCap = 0.5;
-    private int oldSize;
     private static final int radix = 256;
-    public HashMap(){
+    // Constructor when no table size is given so the default table size is used
+    public HashMap() {
         this.tableSize = DEFAULT_TABLE_SIZE;
         this.keys = new String[DEFAULT_TABLE_SIZE];
         this.values = new String[DEFAULT_TABLE_SIZE];
     }
-    public HashMap(int tableSize){
+    // Constructor when table size is given
+    public HashMap(int tableSize) {
+        this.tableSize = tableSize;
         this.keys = new String[tableSize];
         this.values = new String[tableSize];
     }
-    void add(String key, String value){
-        if((double) keyNum / tableSize >= maxCap){
+    // Adds key and value pair to hash map
+    public void add(String key, String value) {
+        // Resizes the table if it reaches 50% or more capacity
+        if((double) keyNum / tableSize >= maxCap) {
             resize();
         }
         int index = hash(key);
-        if(keys[index] != null){
+        // Add key and value pair if the current keys index is empty
+        if(keys[index] == null){
             keys[index] = key;
             values[index] = value;
             keyNum++;
         }
-        else {
-            while (keys[index] != null) {
+        else{
+            // Linear probing to counter collisions by moving to the next closes empty index when there is
+            // a collision
+            while(keys[index] != null) {
                 index += 1;
                 index = index % tableSize;
             }
-            keys[index] = key;
-            values[index] = value;
-            keyNum++;
         }
+        // Add the key and value pair and increase the count of keys in the hashmap
+        keys[index] = key;
+        values[index] = value;
+        keyNum++;
     }
-    String get(String key){
+    // Returns the corresponding value of given key snd returns invalid if there isn't one
+    public String get(String key) {
         int index = hash(key);
-        if(keys[index].equals(key)){
-            return values[index];
-        }
-        else{
-            while(keys[index] != null){
-                index +=1;
-                index = index % tableSize;
-                if(keys[index].equals(key)){
-                    return values[index];
-                }
+        // Loops through the keys starting at the index that the key should be at and checks if the
+        // key at the index is the same as the given key
+        while(keys[index] != null) {
+            if(keys[index].equals(key)) {
+                return values[index];
             }
+            index += 1;
+            index = index % tableSize;
         }
         return INVALID;
     }
-    void resize(){
-        oldSize = tableSize;
-        tableSize = oldSize*2;
-        oldKeys = keys;
-        oldVals = values;
+    // Doubles the size of the HashMap
+    public void resize() {
+        int oldSize = tableSize;
+        tableSize = oldSize * 2;
+        String[] oldKeys = keys;
+        String[] oldVals = values;
         keys = new String[tableSize];
         values = new String[tableSize];
+        // Re-adding old keys and values
         for(int i = 0; i < oldSize; i ++ ){
             if(oldKeys[i] != null){
                 add(oldKeys[i], oldVals[i]);
             }
         }
     }
+    // Calculates hash of given key
     public int hash(String Key) {
         int length = Key.length();
+        int hash = 0;
         for (int i = 0; i < length; i++) {
-            this.hash = (hash * radix + Key.charAt(i)) % tableSize;
+            hash = (hash * radix + Key.charAt(i)) % tableSize;
         }
-        return hash;
+        return hash % tableSize;
     }
 
 }
